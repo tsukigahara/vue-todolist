19bbed73187a6a549b2576b930aa4638135c4bc5
tsukigahara <satsuki.tksm@gmail.com>
2022-11-29 19:46:44 +0100

    master 1.0

--- js/myscript.js
+++ js/myscript.js
@@ -9,19 +9,36 @@ const app = Vue.createApp({
         return {
             tasks: [
                 { text: 'Fare i compiti', done: false }, 
-                { text: 'Fare la spesa', done: true }, 
+                { text: 'Fare la spesa', done: false }, 
                 { text: 'Fare il bucato', done: false }
             ],
             input: '',
         }
     },
     methods: {
+        //add new task to tasks
         add(){
             this.tasks.push({ text: this.input, done: false });
             this.input = '';
-            
+
+        },
+        //when clicked checkbox will remove checked task after 5s
+        remove(){
+            setTimeout(this.spliceFromArray, 5000);
+            //non esiste una forma più compatta?
+            //si poteva fare con v-if ?
+        },
+        spliceFromArray(){
+            this.tasks.forEach((element, index) => {
+                if (element.done === true){
+                    this.tasks.splice(index, 1);
+                    console.log(index);
+                }
+            });
         }
+
     },
+    //come faccio un creare un consolelog che mi fa vedere array ogni secondo. Beforemount?
 })
 
 app.mount('#app')
